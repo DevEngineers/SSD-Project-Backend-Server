0d58a8326d676dc715f1859eabd57e3e0328c495
irfan <50577470+sm2irfan@users.noreply.github.com>
2022-11-17 14:54:51 +0530

    last comit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const router = express.Router();
 const indexController = require('../controllers/index');
+const keycloak = require('../keycloak-config').getKeycloak();
 
+router.use(bodyParser.json());
 /* GET home page. */
 router.get('/', indexController.intial);
+router.post('/', keycloak.protect(['worker','manager']),indexController.tlsHandshake);
 
 module.exports = router;
